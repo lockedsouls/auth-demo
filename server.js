c128ad34c3466af651b7efc234c013fc0bc1e92f
lockedsouls <lockedssouls@gmail.com>
2022-11-19 20:09:27 +0200

    Add static views

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express"); const app = express();
+const path = require("path");
+
+app.use(require("cors")());
+app.use(express.json());
+app.use(express.static(path.join(`${__dirname}/views`)))
+
+app.route("/")
+    .get((req, res) => {
+        res.sendFile(path.join(`${__dirname}/views/index.html`));
+    });
+
+app.listen(3000, () => console.log("Server started"));
