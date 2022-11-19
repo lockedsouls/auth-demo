b6b577aef8f63221c875ff910ce38b8dd4ee8636
lockedsouls <lockedssouls@gmail.com>
2022-11-19 23:09:02 +0200

    Implement login logic in javascript

--- views/index.js
+++ views/index.js
@@ -1,3 +1,22 @@
+if (window.localStorage.getItem("client") !== null){
+    document.querySelector("#login-button").onclick = () => console.log("Already logged in");
+}
+
 function foo(){
-    console.log("Clicked");
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    const button = document.querySelector("#login-button");
+    
+    if (username === "" && password === "") console.error("Invalid inputs");
+    else if (username === "") console.error("Invalid username");
+    else if (password === "") console.error("Invalid password");
+    else{
+        window.localStorage.setItem("client", `{"username": ${username}, "password": ${password}}`);
+        button.onclick = () => console.log("Already logged in");
+    }
+}
+
+function LogOut(){
+    window.localStorage.clear();
+    document.querySelector("#login-button").onclick = foo;
 }
